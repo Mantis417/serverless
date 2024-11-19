5cdd53a338d2fcfb4c55eb8eee5baca8f1968812
Mantis417 <mookazure@gmail.com>
2024-11-19 10:17:39 +0100

    commit yml + deploy

---
+++ deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set variables for the template file and stack name
+TEMPLATE_FILE="cloudform.yml"  # Change this to the path of your CloudFormation template file
+STACK_NAME="serverless"  # Change this to your stack name
+
+# Deploy the CloudFormation stack using the variables from .env
+aws cloudformation deploy \
+  --template-file "$TEMPLATE_FILE" \
+  --stack-name "$STACK_NAME" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    GitHubOwner="mantis417" \
+    GitHubRepo="mantis417/serverless" \
+    GitHubBranch="main" \
+
+# Check if deployment was successful
+if [ $? -eq 0 ]; then
+  echo "CloudFormation stack deployed successfully!"
+else
+  echo "Error: CloudFormation stack deployment failed."
+  exit 1
+fi
+
+echo "This is the contact form URL: $(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[0].OutputValue" --output text)"
